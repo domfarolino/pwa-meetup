3a39732653f8289652fc1fac7823ec95b3c45aa8
domfarolino <domfarolino@gmail.com>
2017-02-05 19:49:23 -0500

    SW Passthrough

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,17 @@
-console.log('Message from the Service Worker');
+const VERSION = 1;
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    self.skipWaiting()
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    self.clients.claim()
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
+});
