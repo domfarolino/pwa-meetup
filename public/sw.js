1cbb7e5841bed8e306c793c7cf8f8420273cbb55
Dom Farolino <domfarolino@gmail.com>
2017-02-07 11:05:17 -0500

    Fully offline - stale while revalidate

--- public/sw.js
+++ public/sw.js
@@ -27,13 +27,28 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
+  const includes = Array.prototype.includes.bind(event.request.url);
+  if (includes('localhost:8080') && !includes('partial') && !includes('.')) {
+    return event.respondWith(caches.match(new Request('http://localhost:8080')));
+  }
+
   const cacheResponse = caches.match(event.request);
   const networkResponse = fetch(event.request);
+  const copiedResponse = networkResponse.then(response => response.clone());
 
   const returnPromise = Promise.race([cacheResponse, networkResponse])
       .then(response => response || networkResponse)
       .catch(response => cacheResponse)
-      .then(response => response || new Response(null, {status: 404}))
+      .then(response => response || new Response(null, {status: 404}));
 
   event.respondWith(returnPromise);
+
+  event.waitUntil(
+  caches.open(`static-${VERSION}`)
+    .then(cache => {
+      copiedResponse.then(response => {
+        cache.put(event.request, response);
+      });
+    })
+  );
 });
